f7381ef9bc0655edd8dcf63d56b793454f80b781
Maina Wycliffe <wmmaina7@gmail.com>
2019-09-21 00:13:05 +0300

    initial design

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,164 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
+// you need to create this file with Github Personal Token
+import 'local.dart' show GITHUB_ACCESS_TOKEN;
+
+const String getMyRepositories = r'''
+  query ReadRepositories($nRepositories: Int!) {
+    viewer {
+      repositories(last: $nRepositories) {
+        nodes {
+          __typename
+          id
+          name
+          viewerHasStarred
+        }
+      }
+    }
+  }
+''';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final _httpLink = HttpLink(
+      uri: 'https://api.github.com/graphql',
+    );
 
-    final HttpLink _httpLink = HttpLink(
-    uri: 'https://api.github.com/graphql',
-);
+    final _authLink = AuthLink(
+      getToken: () async => 'Bearer $GITHUB_ACCESS_TOKEN',
+    );
 
-final AuthLink _authLink = AuthLink(
-    getToken: () async => 'Bearer $YOUR_PERSONAL_ACCESS_TOKEN',
-);
+    final _link = _authLink.concat(_httpLink);
 
-final Link _link = _authLink.concat(_httpLink);
-
-final GraphQLClient _client = GraphQLClient(
+    final _client = ValueNotifier(
+      GraphQLClient(
         cache: InMemoryCache(),
         link: _link,
+      ),
     );
- 
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Infine Scrolling Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: GraphQLProvider(
+        client: _client,
+        child: MyHomePage(title: 'Infinite Scrolling Demo'),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
+  final nRepositories = 10;
+
   final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: Center(
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Query(
+              options: QueryOptions(
+                document: getMyRepositories,
+                variables: <String, dynamic>{
+                  'nRepositories': nRepositories,
+                  // set cursor to null so as to start at the beginning
+                  // 'cursor': 10
+                },
+              ),
+              builder: (QueryResult result, {refetch, FetchMore fetchMore}) {
+                if (result.loading && result.data == null) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                if (result.hasErrors) {
+                  return Text('\nErrors: \n  ' + result.errors.join(',\n  '));
+                }
+
+                if (result.data == null && result.errors == null) {
+                  return const Text(
+                      'Both data and errors are null, this is a known bug after refactoring, you might forget to set Github token');
+                }
+
+                final List<dynamic> repositories =
+                    (result.data['search']['nodes'] as List<dynamic>);
+
+                final Map pageInfo = result.data['search']['pageInfo'];
+                final String fetchMoreCursor = pageInfo['endCursor'];
+                FetchMoreOptions opts = FetchMoreOptions(
+                  variables: {'cursor': fetchMoreCursor},
+                  updateQuery: (previousResultData, fetchMoreResultData) {
+                    // this is where you combine your previous data and response
+                    // in this case, we want to display previous repos plus next repos
+                    // so, we combine data in both into a single list of repos
+                    final List<dynamic> repos = [
+                      ...previousResultData['search']['nodes'] as List<dynamic>,
+                      ...fetchMoreResultData['search']['nodes'] as List<dynamic>
+                    ];
+
+                    fetchMoreResultData['search']['nodes'] = repos;
+
+                    return fetchMoreResultData;
+                  },
+                );
+
+                return Expanded(
+                  child: ListView(
+                    children: <Widget>[
+                      for (var repository in repositories)
+                        ListTile(
+                          leading: (repository['viewerHasStarred'] as bool)
+                              ? const Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                )
+                              : const Icon(Icons.star_border),
+                          title: Text(repository['name'] as String),
+                        ),
+                      if (result.loading)
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            CircularProgressIndicator(),
+                          ],
+                        ),
+                      RaisedButton(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text("Load More"),
+                          ],
+                        ),
+                        onPressed: () {
+                          fetchMore(opts);
+                        },
+                      )
+                    ],
+                  ),
+                );
+              },
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
